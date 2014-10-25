e0d1205e87c66ccad26dabef11912240c5d5a2ee
syafiquel <syafiqulsyazwan@gmail.com>
2014-10-25 20:09:05 +0800

    update changes

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 		<meta charset="UTF-8">
 		<title></title>
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-		<link href="/css/ripples.min.css" rel="stylesheet">
-    	<link href="/css/material-wfont.min.css" rel="stylesheet">
+		<link href="css/ripples.min.css" rel="stylesheet">
+    	<link href="css/material-wfont.min.css" rel="stylesheet">
 	</head>
 <body>
 
 <div class="btn-group">
-        <a href="javascript:void(0)" class="btn btn-default">Nama Kumpulan</a>
+        <a href="javascript:void(0)" class="btn btn-default">Anonymous Llama</a>
         <div class="btn-group">
             <a href="javascript:void(0)" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-               --SILA PILIH--
+               --Click Here--
                 <span class="caret"></span>
             </a>
             
@@ -26,10 +26,7 @@
  	
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="material/js/ripples.min.js"></script>
-<script src="material/js/material.min.js"></script>
+<script src="js/ripples.min.js"></script>
+<script src="js/material.min.js"></script>
 </body>
 </html>
-
-
-
